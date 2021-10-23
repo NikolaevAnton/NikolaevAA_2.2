320a6f5e824a0b68ce4738e9f90d8891a55cd079
AANikolaev <mjop41@gmail.com>
2021-10-23 13:23:06 +0300

    логика программы

--- NikolaevAA_2.2/ViewController.swift
+++ NikolaevAA_2.2/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var viewOutlet: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redSliderOutlet: UISlider!
+    @IBOutlet var greenSliderOutlet: UISlider!
+    @IBOutlet var blueSliderOutlet: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        valueSlider()
+    }
+    
+    func valueSlider() {
+        let red = redSliderOutlet.value
+        let green = greenSliderOutlet.value
+        let blue = blueSliderOutlet.value
+        redLabel.text = "К: \(Int(red * 100))"
+        greenLabel.text = "З: \(Int(green * 100))"
+        blueLabel.text = "Г: \(Int(blue * 100))"
+        colorView(red: red, green: green, blue: blue)
+    }
+    
+    func colorView(red: Float, green: Float, blue: Float) {
+        let redCGFloat = CGFloat(red)
+        let greenCGFloat = CGFloat(green)
+        let blueSGFloat = CGFloat(blue)
+        //print("CGFloat red: \(redCGFloat), green: \(greenCGFloat), blue: \(blueSGFloat)")
+        let rgbColor = UIColor(red: redCGFloat, green: greenCGFloat, blue: blueSGFloat, alpha: 1.0)
+        viewOutlet.backgroundColor = rgbColor
     }
 
-
+    @IBAction func redSlider() {
+        valueSlider()
+    }
+    
+    
+    @IBAction func greenSlider() {
+        valueSlider()
+    }
+ 
+    @IBAction func blueSlider() {
+        valueSlider()
+    }
+    
+    
 }
 
